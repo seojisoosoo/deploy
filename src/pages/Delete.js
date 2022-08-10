b33955a39f4281b8d123f2aa6c360b12037ea79b
Seojisoo20191941 <amy0480@naver.com>
2022-08-11 03:00:24 +0900

    feat:fixing delete errors

--- src/pages/Delete.js
+++ src/pages/Delete.js
@@ -5,25 +5,21 @@ import { useLocation, useNavigate, Navigate } from "react-router-dom";
 //axios.delete이용
 
 const Delete = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const { state } = useLocation();
-  // const onSubmit = () => {
-  axios.delete(`http://127.0.0.1:8000/${state.id}/delete`);
-  // .then((res) => res)
-  // .then((res) => {
-  //   console.log("res받았다" + res);
-  //   if (res.ok) {
-  //     alert("삭제완료!");
-  //   }
-  // });
-  // .catch(console.log("error"));
-  //   navigate("/");
-  // };
-  // let returnValue = window.confirm("정말 삭제하시겠어요?");
-  // console.log(1);
+  axios
+    .delete(`http://127.0.0.1:8000/${state.id}/delete`)
+    .then((res) => res)
+    .then((res) => {
+      console.log("res받았다" + res);
+      if (res.ok) {
+        alert("삭제완료!");
+      }
+    })
+    .catch(console.log("error"));
+  navigate("/");
   return (
     <>
-      {/* {returnValue ? onSubmit() : navigate("/")} */}
       <Navigate to="/" />
     </>
   );
